659878662e4dabe865c4a5df42ef69835e66fdc5
Blekkspruter <jlm4508@rit.edu>
2019-03-06 23:25:36 -0500

    Start of a wonderful life

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float mouseSensitivity = 3;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 movHorizontal = transform.right * xMov; // (1, 0, 0)
+        Vector3 movVertical = transform.forward * zMov; // (0, 0, 1)
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed; //Direction multiplied by speed
+
+        motor.Move(velocity);
+
+        // gotta get rotation on a 3d vector (Turning around... on the x)
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * mouseSensitivity;
+        motor.Rotate(rotation);
+
+        // gotta get rotation on a 3d vector (Camera rotation)
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * mouseSensitivity;
+        motor.RotateCamera(cameraRotation);
+
+        if (sprint)
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+    }
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camera_rotation = Vector3.zero;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    //takes a movement vector 
+    public void Move(Vector3 input_velocity)
+    {
+        velocity = input_velocity;
+    }
+
+    //Gets rotation vector
+    public void Rotate(Vector3 input_rotation)
+    {
+        rotation = input_rotation;
+    }
+
+    //Gets camera Rotation Vector
+    public void RotateCamera(Vector3 input_rotation)
+    {
+        camera_rotation = input_rotation;
+    }
+
+    //This runs every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Perform the movement
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        // Rotations go through quaternion?  Vector3's with an imaginary component.  Euler is the x/y we know
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            cam.transform.Rotate(-camera_rotation);
+        }
+    }
+}
